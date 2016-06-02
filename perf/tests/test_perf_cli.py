ca3e1bedad9094af073c72d697fc00febd63c762
Victor Stinner <vstinner@redhat.com>
2016-06-02 15:29:40 +0200

    test_perf_cli: test also individual run

--- perf/tests/test_perf_cli.py
+++ perf/tests/test_perf_cli.py
@@ -5,8 +5,24 @@ import unittest
 import perf
 
 
-class TestTimeit(unittest.TestCase):
-    def test_cli(self):
+class TestPerfCLI(unittest.TestCase):
+    def test_run_result(self):
+        run = perf.RunResult([1.0, 1.5, 2.0], loops=100)
+        json = run.json()
+
+        args = [sys.executable, '-m', 'perf']
+        proc = subprocess.Popen(args,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True)
+        stdout = proc.communicate(json)[0]
+        self.assertEqual(proc.returncode, 0)
+
+        self.assertEqual(stdout.rstrip(),
+                         'Average: 1.50 sec +- 0.50 sec '
+                         '(3 samples x 100 loops)')
+
+    def test_results(self):
         runs = [perf.RunResult([1.0], loops=100),
                 perf.RunResult([1.5], loops=100),
                 perf.RunResult([2.0], loops=100)]
