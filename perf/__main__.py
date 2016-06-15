b24f85c7acf7398fa39bdb6e95602d0ec4dd0ae6
Victor Stinner <vstinner@redhat.com>
2016-06-15 15:27:05 +0200

    fix hist

--- perf/__main__.py
+++ perf/__main__.py
@@ -231,7 +231,7 @@ def display_histogram_text(args, result):
     count_max = max(counter.values())
     count_width = len(str(count_max))
 
-    line = '%s: %s #' % (bench._format_sample(avg), count_max)
+    line = '%s: %s #' % (result._format_sample(avg), count_max)
     width = columns - len(line)
     if not args.extend:
         width = min(width, 79)
@@ -240,7 +240,7 @@ def display_histogram_text(args, result):
     for ms in range(min(counter), max(counter)+1):
         count = counter.get(ms, 0)
         linelen = int(round(count * line_k))
-        text = bench._format_sample(float(ms) * sample_k)
+        text = result._format_sample(float(ms) * sample_k)
         line = ('#' * linelen) or '|'
         print("{}: {:>{}} {}".format(text, count, count_width, line))
 
