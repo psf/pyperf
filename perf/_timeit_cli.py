98849eb6df42bef7d707907bd4c69a9d7aec5cf3
Victor Stinner <vstinner@redhat.com>
2017-03-01 18:16:33 +0100

    Remove --compare-to implementation from timeit

--- perf/_timeit_cli.py
+++ perf/_timeit_cli.py
@@ -3,10 +3,6 @@
 """
 from __future__ import division, print_function, absolute_import
 
-import sys
-
-from perf._cli import display_title, format_checks
-from perf._utils import get_python_names, abs_executable
 from perf._runner import Runner
 from perf._timeit import bench_timeit
 
@@ -55,73 +51,13 @@ class TimeitRunner(Runner):
     def _process_args(self):
         Runner._process_args(self)
         args = self.args
-        if args.compare_to:
-            args.compare_to = abs_executable(args.compare_to)
 
         self._show_name = bool(args.name)
         if not args.name:
             args.name = DEFAULT_NAME
 
-    def bench_compare(self, python, loops):
-        args = self.args
-        # FIXME: it's no more needed to manually save/restore loops
-        args.loops = loops
-        args.python = python
-        args.compare = None
-        return self._spawn_workers(newline=False)
-
-
-def cmd_compare(runner):
-    from perf._compare import timeit_compare_benchs
-
-    args = runner.args
-    for option in ('output', 'append', 'worker'):
-        if getattr(args, option):
-            print("ERROR: --%s option is not supported in compare mode"
-                  % option)
-            sys.exit(1)
-
-    # need a local copy, because bench_compare() modifies args
-    loops = args.loops
-    python1 = args.compare_to
-    python2 = args.python
-    name1, name2 = get_python_names(python1, python2)
-
-    multiline = runner._multiline_output()
-
-    benchs = []
-    for python, name in ((python1, name1), (python2, name2)):
-        if multiline:
-            display_title('Benchmark %s' % name)
-        elif not args.quiet:
-            print(name, end=': ')
-
-        bench = runner.bench_compare(python, loops)
-        benchs.append(bench)
-        if multiline:
-            runner._display_result(bench)
-        elif not args.quiet:
-            print(' %s' % bench.format())
-
-        if multiline:
-            print()
-        elif not args.quiet:
-            warnings = format_checks(bench)
-            for line in warnings:
-                print(line)
-
-    bench1, bench2 = benchs
-    if multiline:
-        display_title('Compare')
-    elif not args.quiet:
-        print()
-    timeit_compare_benchs(name1, bench1, name2, bench2, args)
-
 
 def main(runner):
-    if runner.args.compare_to:
-        cmd_compare(runner)
-    else:
-        args = runner.args
-        bench_timeit(runner, args.name, args.stmt, args.setup,
-                     args.inner_loops, args.duplicate)
+    args = runner.args
+    bench_timeit(runner, args.name, args.stmt, args.setup,
+                 args.inner_loops, args.duplicate)
