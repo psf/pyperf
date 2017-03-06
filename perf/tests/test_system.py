2b5f4a2c9d604441c71cab54ffb57e47ac23aa96
Victor Stinner <vstinner@redhat.com>
2017-03-06 17:51:09 +0100

    test_system: more info on failure

--- perf/tests/test_system.py
+++ perf/tests/test_system.py
@@ -9,13 +9,14 @@ class SystemTests(unittest.TestCase):
     def test_show(self):
         args = [sys.executable, '-m', 'perf', 'system', 'show']
         proc = get_output(args)
-        self.assertEqual(proc.returncode, 0)
 
         regex = ('(Run "%s -m perf system tune" to tune the system configuration to run benchmarks'
                  '|OK! System ready for benchmarking'
                  '|WARNING: no operation available for your platform)'
                  % os.path.basename(sys.executable))
-        self.assertRegex(proc.stdout, regex)
+        self.assertRegex(proc.stdout, regex, msg=proc)
+
+        self.assertEqual(proc.returncode, 0, msg=proc)
 
 
 if __name__ == "__main__":
