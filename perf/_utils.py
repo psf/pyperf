81ab610741b99c391f5375f9b2495d3015b37768
Victor Stinner <victor.stinner@gmail.com>
2017-04-13 14:48:53 +0200

    which(): use lazy import for 'import shutil'

--- perf/_utils.py
+++ perf/_utils.py
@@ -265,72 +265,60 @@ def get_python_names(python1, python2):
     return (python1, python2)
 
 
-try:
-    # Python 3.3
-    from shutil import which
-except ImportError:
-    # Backport shutil.which() from Python 3.6
-    def which(cmd, mode=os.F_OK | os.X_OK, path=None):
-        """Given a command, mode, and a PATH string, return the path which
-        conforms to the given mode on the PATH, or None if there is no such
-        file.
+_which = None
 
-        `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
-        of os.environ.get("PATH"), or can be overridden with a custom search
-        path.
 
-        """
-        # Check that a given file can be accessed with the correct mode.
-        # Additionally check that `file` is not a directory, as on Windows
-        # directories pass the os.access check.
-        def _access_check(fn, mode):
-            return (os.path.exists(fn) and os.access(fn, mode)
-                    and not os.path.isdir(fn))
+def which(*args, **kw):
+    # Wrapper to which() to use lazy import for 'import shutil'
+    global _which
 
-        # If we're given a path with a directory part, look it up directly rather
-        # than referring to PATH directories. This includes checking relative to the
-        # current directory, e.g. ./script
-        if os.path.dirname(cmd):
-            if _access_check(cmd, mode):
-                return cmd
-            return None
+    if _which is None:
+        try:
+            # Python 3.3
+            from shutil import which as _which
+        except ImportError:
+            # Backport shutil.which() from Python 3.6,
+            # comments/docstring stripped
+            def _which(cmd, mode=os.F_OK | os.X_OK, path=None):
+                def _access_check(fn, mode):
+                    return (os.path.exists(fn) and os.access(fn, mode)
+                            and not os.path.isdir(fn))
 
-        if path is None:
-            path = os.environ.get("PATH", os.defpath)
-        if not path:
-            return None
-        path = path.split(os.pathsep)
+                if os.path.dirname(cmd):
+                    if _access_check(cmd, mode):
+                        return cmd
+                    return None
 
-        if sys.platform == "win32":
-            # The current directory takes precedence on Windows.
-            if os.curdir not in path:
-                path.insert(0, os.curdir)
+                if path is None:
+                    path = os.environ.get("PATH", os.defpath)
+                if not path:
+                    return None
+                path = path.split(os.pathsep)
 
-            # PATHEXT is necessary to check on Windows.
-            pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
-            # See if the given file matches any of the expected path extensions.
-            # This will allow us to short circuit when given "python.exe".
-            # If it does match, only test that one, otherwise we have to try
-            # others.
-            if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
-                files = [cmd]
-            else:
-                files = [cmd + ext for ext in pathext]
-        else:
-            # On other platforms you don't have things like PATHEXT to tell you
-            # what file suffixes are executable, so just pass on cmd as-is.
-            files = [cmd]
+                if sys.platform == "win32":
+                    if os.curdir not in path:
+                        path.insert(0, os.curdir)
 
-        seen = set()
-        for dir in path:
-            normdir = os.path.normcase(dir)
-            if normdir not in seen:
-                seen.add(normdir)
-                for thefile in files:
-                    name = os.path.join(dir, thefile)
-                    if _access_check(name, mode):
-                        return name
-        return None
+                    pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+                    if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+                        files = [cmd]
+                    else:
+                        files = [cmd + ext for ext in pathext]
+                else:
+                    files = [cmd]
+
+                seen = set()
+                for dir in path:
+                    normdir = os.path.normcase(dir)
+                    if normdir not in seen:
+                        seen.add(normdir)
+                        for thefile in files:
+                            name = os.path.join(dir, thefile)
+                            if _access_check(name, mode):
+                                return name
+                return None
+
+    return _which(*args, **kw)
 
 
 def abs_executable(python):
