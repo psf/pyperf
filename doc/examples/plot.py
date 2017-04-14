b9c7d3f76dee20dd1da1e53365b2aeac616bd0a3
Victor Stinner <victor.stinner@gmail.com>
2017-04-14 11:41:01 +0200

    plot: add --run option

--- doc/examples/plot.py
+++ doc/examples/plot.py
@@ -6,41 +6,53 @@ import statistics
 
 def plot_bench(args, bench):
     if not args.split_runs:
-        values = bench.get_values()
-        if args.skip:
-            values = values[args.skip:]
-        values = [value for value in values]
+        runs = bench.get_runs()
+        if args.run:
+            run = runs[args.run - 1]
+            runs = [run]
+        values = []
+        for run in runs:
+            run_values = run.values
+            if args.skip:
+                run_values = run_values[args.skip:]
+            values.extend(run_values)
         plt.plot(values, label='values')
 
         mean = statistics.mean(values)
         plt.plot([mean] * len(values), label='mean')
-
-        plt.legend(loc='upper right', shadow=True, fontsize='x-large')
     else:
+        values = []
+        width = None
         for run_index, run in enumerate(bench.get_runs()):
             index = 0
             x = []
             y = []
-            values = run.values
+            run_values = run.values
             if args.skip:
-                values = values[args.skip:]
-            for value in values:
+                run_values = run_values[args.skip:]
+            for value in run_values:
                 x.append(index)
                 y.append(value)
                 index += 1
             plt.plot(x, y, color='blue')
+            values.extend(run_values)
+            width = len(run_values)
 
             if args.warmups:
-                values = [value for loops, value in run.warmups]
+                run_values = [value for loops, value in run.warmups]
                 index = -len(run.warmups) + 1
                 x = []
                 y = []
-                for value in values:
+                for value in run_values:
                     x.append(index)
                     y.append(value)
                     index += 1
                 plt.plot(x, y, color='red')
 
+        mean = statistics.mean(values)
+        plt.plot([mean] * width, label='mean', color='green')
+
+    plt.legend(loc='upper right', shadow=True, fontsize='x-large')
     plt.show()
 
 
@@ -50,6 +62,8 @@ def parse_args():
     parser.add_argument('--split-runs', action='store_true')
     parser.add_argument('--skip', type=int, help='skip first SKIP values')
     parser.add_argument('--warmups', action='store_true')
+    parser.add_argument('--run', metavar='INDEX', type=int,
+                        help='only render run number INDEX')
     parser.add_argument('filename')
     return parser.parse_args()
 
