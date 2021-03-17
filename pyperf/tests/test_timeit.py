219567913b9e662326ec260a8247f6a0f920c2b8
Dong-hee Na <donghee.na@python.org>
2021-03-18 01:05:40 +0900

    Add Windows CI

--- pyperf/tests/test_timeit.py
+++ pyperf/tests/test_timeit.py
@@ -222,6 +222,8 @@ class TestTimeit(unittest.TestCase):
     # When the PyPy program is copied, it fails with "Library path not found"
     @unittest.skipIf(pyperf.python_implementation() == 'pypy',
                      'pypy program cannot be copied')
+    @unittest.skipIf(sys.platform == 'win32',
+                     'https://github.com/psf/pyperf/issues/97')
     def test_python_option(self):
         # Ensure that paths are absolute
         paths = [os.path.realpath(path) for path in sys.path]
