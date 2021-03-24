cc6a62ea7df6d69d15ae987a8a18eabfdac63551
Dong-hee Na <donghee.na@python.org>
2021-03-25 00:53:39 +0900

    post-release (#104)

--- pyperf/__init__.py
+++ pyperf/__init__.py
@@ -1,6 +1,6 @@
 from time import perf_counter
 
-VERSION = (2, 2, 0)
+VERSION = (2, 2, 1)
 __version__ = '.'.join(map(str, VERSION))
 
 # Export pyperf.perf_counter for backward compatibility with pyperf 1.7
