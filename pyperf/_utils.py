127e0bda5a74d59bc7a305236b5fa725a848fd1b
Donghee Na <donghee.na@python.org>
2024-05-14 02:59:44 +0900

    gh-187: Support environment variables PYTHON_GIL / PYTHON_CPU_COUNT (#188)
    * gh-187: Support environment variables PYTHON_GIL / PYTHON_CPU_COUNT
    
    * Update
    
    * Update CI
    
    * update
    
    * Update
    
    * Address code review
    
    * Address code review
    
    * Revert CI file

--- pyperf/_utils.py
+++ pyperf/_utils.py
@@ -266,8 +266,9 @@ def create_environ(inherit_environ, locale, copy_all):
     if copy_all:
         return os.environ
     env = {}
-
-    copy_env = ["PATH", "PYTHONPATH", "HOME", "TEMP", "COMSPEC", "SystemRoot", "SystemDrive"]
+    copy_env = ["PATH", "HOME", "TEMP", "COMSPEC", "SystemRoot", "SystemDrive",
+                # Python specific variables
+                "PYTHONPATH", "PYTHON_CPU_COUNT", "PYTHON_GIL"]
     if locale:
         copy_env.extend(('LANG', 'LC_ADDRESS', 'LC_ALL', 'LC_COLLATE',
                          'LC_CTYPE', 'LC_IDENTIFICATION', 'LC_MEASUREMENT',
